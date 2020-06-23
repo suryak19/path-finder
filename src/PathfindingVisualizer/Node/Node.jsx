cb854d091f41b142ec763bb6208dda5f3acc9b19
suryak19 <surya.konduru19@gmail.com>
2020-06-23 17:50:31 +0530

    PATH-FIND-002 Set start and End points

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import './Node.css';
 
-export default class Node extends Component{
-    constructor(props){
+export default class Node extends Component {
+    constructor(props) {
         super(props);
         this.state = {};
     }
 
-    render(){
-        return <div className="node"></div>;
+    render() {
+        const {isFinish, isStart} = this.props;
+        const extraClassName = isFinish ? 'node-finish' : isStart ? 'node-start' : '';
+        return <div className={`node ${extraClassName}`}></div>;
     }
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -17,7 +17,13 @@ export default class PathfindingVisualizer extends Component {
         for (let row = 0; row < 20; row++) {
             const currentRow = [];
             for (let col = 0; col < 50; col++) {
-                currentRow.push([]);
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col === 45
+                };
+                currentRow.push(currentNode);
             }
             nodes.push(currentRow);
         }
@@ -28,12 +34,22 @@ export default class PathfindingVisualizer extends Component {
         const {nodes} = this.state;
         console.log(nodes);
         return (
-            <div key>
+            <div className="grid">
                 {
                 nodes.map((row, rowIdx) => {
                     return (
-                        <div>{
-                            row.map((node, nodeIdx) => <Node></Node>)
+                        <div key={rowIdx}>
+                            {
+                            row.map((node, nodeIdx) => {
+                                const {isStart,isFinish} = node;
+                                return (
+                                    <Node key={nodeIdx}
+                                        isStart={isStart}
+                                        isFinish={isFinish}
+                                        test={'foo'}
+                                        test={'kappa'}></Node>
+                                )
+                            })
                         } </div>
                     )
                 })

6ac9d2c9f5411b6d0250da339335a5c118ef7721
suryak19 <surya.konduru19@gmail.com>
2020-06-23 12:33:55 +0530

    PATH-FIND-001 Basic skeleton code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
 function App() {
   return (

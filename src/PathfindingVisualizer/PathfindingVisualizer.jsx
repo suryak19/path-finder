3b463d9cdbd8f56d339e489768b90e9ec2bf9181
suryak19 <surya.konduru19@gmail.com>
2020-06-23 17:28:26 +0530

    PATH-FIND-002 Grid construction

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Node from './Node/Node';
 
 import './PathfindingVisualizer.css';
@@ -6,15 +6,38 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nodes: []
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+
+        for (let row = 0; row < 20; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
     }
 
     render() {
+        const {nodes} = this.state;
+        console.log(nodes);
         return (
-            <div>
-                Foo
-                <Node></Node>
-            </div>
+            <div key>
+                {
+                nodes.map((row, rowIdx) => {
+                    return (
+                        <div>{
+                            row.map((node, nodeIdx) => <Node></Node>)
+                        } </div>
+                    )
+                })
+            } </div>
         );
     }
 }
